7e2f04138ca412f45c0bc411ef7e3983f5ea342d
Joshua Manela <jmmanela@mtu.edu>
2014-08-13 14:28:12 -0400

    changed comments

--- real-time-utils/IRLReg.m
+++ real-time-utils/IRLReg.m
@@ -1,7 +1,7 @@
 function [m, b, R2] = IRLReg(x,y)
 
-        %This is code with finds a regression between the x and y.  It also
-        %returns the slope and intercept, plus the R^2 from the regression.
+%         This is code with finds a regression between the x and y.  It also
+%         returns the slope and intercept, plus the R^2 from the regression.
 
         p = polyfit(x,y,1);
         
